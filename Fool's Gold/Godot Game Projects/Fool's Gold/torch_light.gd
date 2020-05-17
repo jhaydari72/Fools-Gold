29c748d8f3acd254c1a39cdb1faf1652a0427ba8
jhaydari72 <jhaydari72@gmail.com>
2020-05-16 19:28:07 -0700

    Torch works properly

--- Fool's Gold/Godot Game Projects/Fool's Gold/torch_light.gd
+++ Fool's Gold/Godot Game Projects/Fool's Gold/torch_light.gd
@@ -1,14 +1,5 @@
 extends Light2D
-var torch_light = false
+
+#starts the light turned off
 func _ready():
 	hide()
-
-func _input (event):
-	if torch_light == false:
-		if Input.is_action_just_pressed("ui_enter"):
-			torch_light = true
-			show()
-	elif torch_light == true:
-		if Input.is_action_just_pressed("ui_enter"):
-			torch_light = false
-			hide()
